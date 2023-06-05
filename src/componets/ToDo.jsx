d9a7047399042bdf135cd5514b3f785de33d32b9
f <franklinrohan2002@gmail.com>
2023-06-05 15:22:33 +0530

    chore:made a delete button

--- src/componets/ToDo.jsx
+++ src/componets/ToDo.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 function ToDo({ todos, setTodos }) {
+  const deleteTask = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div>
       {todos.map((todo) => (
@@ -17,7 +21,11 @@ function ToDo({ todos, setTodos }) {
             <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2">
               Mark
             </button>
-            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => deleteTask(todo.id)}>
+              {" "}
+              {/* Pass todo.id as the parameter */}
               Delete
             </button>
           </div>
