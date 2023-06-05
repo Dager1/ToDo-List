c8661ff725d4ae2988d4f85f1161ed251ab62d60
f <franklinrohan2002@gmail.com>
2023-06-05 10:20:16 +0530

    style:added border and changed the font

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import Input from "./componets/Input";
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="mx-auto max-w-lg p-4 border border-blue-500 rounded-lg h-80">
+      <div>
+        <h1 className="text-3xl font-bold underline mb-4">TODO-LIST</h1>
+      </div>
+      <div>
+        <Input />
+      </div>
     </div>
   );
 }
--- src/componets/Input.jsx
+++ src/componets/Input.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Input() {
-  cosnt[(task, setTask)] = setState("");
+  const [task, setTask] = useState("");
+
   return (
     <div>
       <form>
         <input
+          className="border border-gray-300 px-4 py-2 rounded-md"
           type="text"
           value={task}
+          required
           onChange={(e) => setTask(e.target.value)}
         />
-        <button>Sumbit</button>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2">
+          Submit
+        </button>
       </form>
     </div>
   );
