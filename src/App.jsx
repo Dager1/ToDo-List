5fd1965ccd568b25456e0ea35612bd865448aed2
f <franklinrohan2002@gmail.com>
2023-06-05 11:30:53 +0530

    style:added border and added color to button

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import "./App.css";
 import Input from "./componets/Input";
 function App() {
   return (
-    <div className="mx-auto max-w-lg p-4 border border-blue-500 rounded-lg h-80">
+    <div className="mx-auto max-w-lg p-4 border border-blue-500 rounded-lg h-80 flex flex-grow flex-col">
       <div>
         <h1 className="text-3xl font-bold underline mb-4">TODO-LIST</h1>
       </div>
-      <div>
+      <div className="flex-grow">
         <Input />
       </div>
     </div>
--- src/componets/ToDo.jsx
+++ src/componets/ToDo.jsx
@@ -4,15 +4,22 @@ function ToDo({ todos, setTodos }) {
   return (
     <div>
       {todos.map((todo) => (
-        <li key={todo.id}>
+        <li key={todo.id} className="border rounded-lg p-4 mb-4">
           <input
             value={todo.title}
-            onChange={(event) => event.preventDefault}
+            onChange={(event) => event.preventDefault()}
+            className="border rounded-lg p-2"
           />
-          <div>
-            <button>Edit</button>
-            <button>Mark</button>
-            <button>Delet</button>
+          <div className="mt-2">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+              Edit
+            </button>
+            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2">
+              Mark
+            </button>
+            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+              Delete
+            </button>
           </div>
         </li>
       ))}
