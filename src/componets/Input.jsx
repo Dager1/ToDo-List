dcf976e57bd94cceada62c1ae36b0f04d1205608
f <franklinrohan2002@gmail.com>
2023-06-05 10:47:18 +0530

    chore:added an array to store the todos

--- src/componets/Input.jsx
+++ src/componets/Input.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function Input() {
   const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
 
+  const formSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, { id: uuidv4, title: task, completed: false }]);
+    setTask("");
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={formSubmit}>
         <input
           className="border border-gray-300 px-4 py-2 rounded-md"
           type="text"
